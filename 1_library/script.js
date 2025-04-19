d59bc14ecb4bd619aa64d835098e7026913c7f3b
EliasBach <el.bach1999@gmail.com>
2025-04-19 20:35:04 +0200

    adds constructor and function to add and display books

--- 1_library/script.js
+++ 1_library/script.js
@@ -1,21 +1,34 @@
+const myLibrary = [];
+const container = document.querySelector('#container')
+
 function Book(title, author, pages, read) {
-    if (!new.target) {
-      throw Error("Use 'new' operator when calling a constructor")
-    }
-    this.title = title; // string
-    this.author = author; // string
-    this.pages = pages // number
-    this.read = read // boolean
-    this.info = function() {
-        return `${this.title} by ${this.author}, 
-        ${this.pages} pages, ${read ? "read" : "not yet read"}`
-    }
+  if (!new.target) {
+    throw Error("Use 'new' operator when calling a constructor")
   }
+  this.title = title; // string
+  this.author = author; // string
+  this.pages = pages // number
+  this.read = read // boolean
+  this.id = crypto.randomUUID()
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${read ? "read" : "not yet read"}`
+  }
+}
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, false)
-console.log(theHobbit.info())
-console.log(Object.getPrototypeOf(theHobbit))
-console.log(Book.prototype)
-console.log(theHobbit.valueOf());
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read))
+}
 
-// Obj1.prototype = Obj2.prototype; does not work!
+function displayLibrary() {
+  myLibrary.forEach((book) => {
+    const para = document.createElement("p");
+    para.textContent = book.info()
+  container.appendChild(para)
+  })
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, false)
+addBookToLibrary('The Bible', "God", 420, false)
+addBookToLibrary('1984', "George Orwell", 328, true)
+
+displayLibrary()
