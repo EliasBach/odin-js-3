de5d28cde0b1a036c966085ab23e94cbc3e69d41
EliasBach <el.bach1999@gmail.com>
2025-04-20 11:32:04 +0200

    adds disable function to add-book-btn

--- 1_library/script.js
+++ 1_library/script.js
@@ -1,9 +1,33 @@
 const myLibrary = [];
 const container = document.querySelector('#container')
-const addBookBtn = document.querySelector('#AddBookBtn')
+const bookSubmitForm = document.querySelector('#BookSubmitForm')
+const book_inputs = ["title", "author", "num-pages"]
+const book_inputs_types = ["text", "text", "number"]
+
+const addBookBtn = document.querySelector('.AddBookBtn')
 addBookBtn.addEventListener('click', () => {
-  // creates Form
+  // create input element for each datapoint needed to save book
+  book_inputs.forEach(function(el) {
+    let new_input = document.createElement("input")
+    new_input.placeholder = el
+    bookSubmitForm.appendChild(new_input)
+  })
+  // temporarily disable the button, so that more inputs cannot be made
+  addBookBtn.disabled = true
+
+  // create a submit-book button
+  let submitBookBtn = document.createElement("button")
+  submitBookBtn.setAttribute("class", "SubmitBookBtn")
+  submitBookBtn.textContent = "submit"
+  submitBookBtn.addEventListener("click", () => {
+    
+  })
+  bookSubmitForm.appendChild(submitBookBtn)
 })
+
+function addBookToLibrary(title, author, pages, read=false) {
+  myLibrary.push(new Book(title, author, pages, read))
+}
 
 function Book(title, author, pages, read) {
   if (!new.target) {
@@ -19,24 +43,23 @@ function Book(title, author, pages, read) {
   }
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read))
-}
-
 function displayLibrary() {
   myLibrary.forEach((book) => {
     const entry = document.createElement("li");
     entry.textContent = book.info()
     
     // remove button functionality
-    const remove_button = document.createElement("button")
-    remove_button.setAttribute("class", "RemoveBtn")
-    entry.appendChild(remove_button)
-    remove_button.textContent = "remove"
-    remove_button.addEventListener('click', () => {
+    const RemoveBtn = document.createElement("button")
+    RemoveBtn.setAttribute("class", "RemoveBtn")
+    entry.appendChild(RemoveBtn)
+    RemoveBtn.textContent = "remove"
+    RemoveBtn.addEventListener('click', () => {
       container.removeChild(entry);
     });
-    
+
+    // read toggle will go here
+    //
+
     container.appendChild(entry)
   })
 }
