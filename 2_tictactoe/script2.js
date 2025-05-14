4588f7748012526c0d1f562809b99ace5b1a0de0
EliasBach <el.bach1999@gmail.com>
2025-05-14 11:41:14 +0200

    game can initialise and playMove function implement

--- 2_tictactoe/script2.js
+++ 2_tictactoe/script2.js
@@ -1,1 +1,31 @@
+const TicTacToe = (function() {
+    // private state
+    
+    let board = [
+        [".", ",", "."],
+        [",", "+", ","],
+        [".", ",", "."]]
+    let turnCounter = 0
+    let markers = ["X", "O"]
+    
+    // private methods
 
+
+    // public methods (incl. getters and board actions)
+    return { 
+        getBoard: function() {
+            return board
+        },
+        
+        playMove: function(row, col) {
+            board[row][col] = markers[0]
+        }
+    }
+})();
+
+// testing game flow
+console.table(TicTacToe.getBoard())
+TicTacToe.playMove(1,2)
+console.table(TicTacToe.getBoard())
+TicTacToe.playMove(2,2)
+console.table(TicTacToe.getBoard())
