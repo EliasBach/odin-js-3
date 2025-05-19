93d6cd9ba3bf5d4fffd449943995af21498d3781
EliasBach <el.bach1999@gmail.com>
2025-05-19 19:44:53 +0200

    webpack practice pt 2

--- webpack-practice/src/index.js
+++ webpack-practice/src/index.js
@@ -3,3 +3,11 @@ import "./styles.css";
 import { greeting } from "./greeting.js";
 
 console.log(greeting);
+
+
+import odinImage from "./odin.svg";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
