d6d4eded678fd19fbf77ee8c6e73fe223e801300
EliasBach <el.bach1999@gmail.com>
2025-05-20 09:23:27 +0200

    setup modules for each of the tabs

--- 3_restaurant-page/src/index.js
+++ 3_restaurant-page/src/index.js
@@ -1,2 +1,8 @@
 import "./styles.css";
 alert("test")
+
+// tab-switching logic will go here:
+// use event listeners for each button in the header navbar 
+// that wipes out the current contents of div#content 
+// and then runs the correct ‘tab module’ 
+// to populate it with the new contents again
