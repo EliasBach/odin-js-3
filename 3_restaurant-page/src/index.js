44f4e613a76c82956525c8b353319faa10e4d9eb
EliasBach <el.bach1999@gmail.com>
2025-05-20 17:02:27 +0200

    creating first elements: buttons for tabs

--- 3_restaurant-page/src/index.js
+++ 3_restaurant-page/src/index.js
@@ -1,6 +1,19 @@
 import "./styles.css";
-alert("test")
+import "./tab_home"
+import "./tab_menu"
+import "./tab_about"
 
+const homeBtn = document.createElement("button")
+homeBtn.textContent = "Home"
+const menuBtn = document.createElement("button")
+menuBtn.textContent = "Menu"
+const aboutBtn = document.createElement("button")
+aboutBtn.textContent = "About"
+
+const nav = document.querySelector("nav")
+nav.appendChild(homeBtn)
+nav.appendChild(menuBtn)
+nav.appendChild(aboutBtn)
 // tab-switching logic will go here:
 // use event listeners for each button in the header navbar 
 // that wipes out the current contents of div#content 
---
+++ 3_restaurant-page/src/tab_contact.js
@@ -0,0 +1,3 @@
+// module contains code to create DOM elements
+// to display info in the about tab
+// about tab contains: contact infos
