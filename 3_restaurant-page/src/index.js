2a1b718549455ae6804b2d4920978a5adfc148c1
EliasBach <el.bach1999@gmail.com>
2025-05-22 20:49:59 +0200

    tab switching logic implemented

--- 3_restaurant-page/src/index.js
+++ 3_restaurant-page/src/index.js
@@ -1,28 +1,46 @@
 import "./styles.css";
-import homeTab from "./tab_home"
-import menuTab from "./tab_menu"
-import contactTab from "./tab_contact"
 
+// header at the very top of the page
 const header = document.querySelector("header")
 header.textContent = "Welcome to the Odin Restaurant Site"
 
+// navigation bar, with buttons that activate each of the tabs
 const nav = document.querySelector("nav")
-const content = document.querySelector("#content")
-
 const homeBtn = document.createElement("button")
 homeBtn.textContent = "Home"
+homeBtn.addEventListener("click", create_homeTab)
+homeBtn.setAttribute("id", "homeBtn")
 const menuBtn = document.createElement("button")
 menuBtn.textContent = "Menu"
+menuBtn.addEventListener("click", create_menuTab)
+menuBtn.setAttribute("id", "menuBtn")
 const contactBtn = document.createElement("button")
 contactBtn.textContent = "Contact"
-
+contactBtn.addEventListener("click", create_contactTab)
+contactBtn.setAttribute("id", "contactBtn")
 nav.appendChild(homeBtn)
 nav.appendChild(menuBtn)
 nav.appendChild(contactBtn)
 
-content.append(homeTab)
-// tab-switching logic will go here:
-// use event listeners for each button in the header navbar 
-// that wipes out the current contents of div#content 
-// and then runs the correct ‘tab module’ 
-// to populate it with the new contents again
+// tab-switching logic 
+// each button calls a function that wipes out 
+// the current contents of div#content 
+// and populates it with the content defined in corresponding module
+const content = document.querySelector("#content")
+
+import homeTab from "./tab_home"
+import menuTab from "./tab_menu"
+import contactTab from "./tab_contact"
+
+function create_homeTab() {
+    content.innerHTML = ""
+    content.append(homeTab)
+}
+function create_menuTab() {
+    content.innerHTML = ""
+    content.append(menuTab)
+}
+function create_contactTab() {
+    content.innerHTML = ""
+    content.append(contactTab)
+}
--- 3_restaurant-page/src/tab_home.js
+++ 3_restaurant-page/src/tab_home.js
@@ -1,7 +1,10 @@
-// module contains code to create DOM elements
-// to display info in the about tab
+// creates the content for the home tab
 const tab = document.createElement("div")
 tab.classList.add("tab")
+
+// description
+const sec1 = document.createElement("div")
+sec1.classList.add("section")
 
 const main_header = document.createElement("div")
 main_header.classList.add("header")
@@ -9,14 +12,45 @@ main_header.setAttribute("id", "main_header")
 main_header.textContent = "The Odin Restaurant"
 
 const description = document.createElement("div")
-description.setAttribute("id", "description");
+description.classList.add("text")
+description.setAttribute("id", "description")
 description.textContent = "Welcome to Odin’s! Where timeless Norse tradition meets modern culinary excellence. Nestled in the heart of the city, we offer a bold, flavorful menu inspired by Viking heritage and crafted with fresh, locally sourced ingredients. From hearty meats and fire-grilled seafood to decadent desserts and artisan meads, every dish is a feast worthy of the gods. Our rustic yet elegant atmosphere invites you to gather, celebrate, and indulge. Whether you’re here for a casual meal or a grand occasion, Odin’s promises an unforgettable dining experience rooted in legend and delivered with passion. Join us and dine like a warrior."
 
+sec1.appendChild(main_header)
+sec1.appendChild(description)
+
 // opening hours
+const sec2 = document.createElement("div")
+sec2.classList.add("section")
+
+const hours_header = document.createElement("div")
+hours_header.classList.add("header")
+hours_header.textContent = "Opening Hours"
+
+const hours_text = document.createElement("div")
+hours_text.classList.add("text")
+hours_text.textContent = "Monday - Friday: 12:00 til 22:00   |   Saturday - Sunday: 16:00 til 23:00"
+
+sec2.appendChild(hours_header)
+sec2.appendChild(hours_text)
 
 // address
+const sec3 = document.createElement("div")
+sec3.classList.add("section")
 
-tab.appendChild(main_header)
-tab.appendChild(description)
+const address_header = document.createElement("div")
+address_header.classList.add("header")
+address_header.textContent = "Address"
+
+const address_text = document.createElement("div")
+address_text.classList.add("text")
+address_text.textContent = "Freya Boulevard 99, 474NO Oslo, Norway"
+
+sec3.appendChild(address_header)
+sec3.appendChild(address_text)
+
+tab.appendChild(sec1)
+tab.appendChild(sec2)
+tab.appendChild(sec3)
 
 export default tab
